d61e953c4aba520017a37e32c9cee61e777cccaa
Chaitanya Deepthi <chdeepthidolie@gmail.com>
2026-05-12 13:43:14 -0700

    Consider the UpsertViewManager when consistency mode is enabled to make sure we don't see inconsistencies in queries

--- pinot-segment-local/src/main/java/org/apache/pinot/segment/local/upsert/UpsertUtils.java
+++ pinot-segment-local/src/main/java/org/apache/pinot/segment/local/upsert/UpsertUtils.java
@@ -74,9 +74,6 @@ public class UpsertUtils {
     if (snapshot != null) {
       return snapshot.isEmpty();
     }
-    if (segment.isUpsertConsistencyModeEnabled()) {
-      return false;
-    }
     ThreadSafeMutableRoaringBitmap queryableDocIds = segment.getQueryableDocIds();
     if (queryableDocIds != null) {
       return queryableDocIds.isEmpty();
